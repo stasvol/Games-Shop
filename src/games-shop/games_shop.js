951cd92dad16ca42238a34faef8394a2dc9c5c12
stasvol <https://github.com/stasvol/my-test.git>
2021-06-29 16:20:36 +0300

    update file

--- src/games-shop/games_shop.js
+++ src/games-shop/games_shop.js
@@ -53,7 +53,6 @@ const ShopGames = () => {
         setSearchResults(stories)
     }
 
-
     const axiosData = (searchQuery) => {
         setSearchQuery(searchQuery)
         axios.get
@@ -62,11 +61,11 @@ const ShopGames = () => {
             .then(res => {
                 const games = res.data
                 setSearchGames(games)
-                console.log(games)
+
             })
             .catch(error => console.error('Error ' + error))
-
     }
+
 
     useEffect(() => {
         // const storiesIds = searchResults
@@ -81,14 +80,14 @@ const ShopGames = () => {
         // })
 
         const storiesIds = searchResults
-            .filter(item => item.isChecked) // [{}, {}]
+            .filter(item => item.isChecked) // [{}, {}] isChecked=true
             .map(item => {
                 return item.storeID;
             })  // [1, 3, 5]
             .join(','); // 1,2,3
 
         axios.get(` https://www.cheapshark.com/api/1.0/deals?storeID=${storiesIds}&title=${searchQuery}`)
-
+            //     (`${BASE_PATH}${PARAM_DEALS}?${PARAM_STORE_ID}${storiesIds}${TITLE_PARAM}${searchQuery}`)
             // .then(res => {
             //     const deals = res.data
             //     setDeals(deals)
@@ -102,6 +101,7 @@ const ShopGames = () => {
 
     const setAxiosDeals = deals => {
         setDeals(deals)
+        console.log(deals)
         // console.log( "DEALS " + deals)
 
     }
@@ -110,7 +110,7 @@ const ShopGames = () => {
     const getSearch = (e) => {
         if (e.key === 'Enter') {
             setSearchQuery(searchQuery)
-            // setSearchGames(searchGames )
+
             axiosData(searchQuery)
 
         }
@@ -175,7 +175,7 @@ const ShopGames = () => {
         {searchGames && searchGames.length
 
             ? searchGames.map((item, i) => (
-                <div key={i}>{item.internalName} </div>))
+                <div key={item.gameID}>{item.internalName} </div>))
 
 
             : <p>Empty game list</p>
@@ -187,7 +187,7 @@ const ShopGames = () => {
         <h3>Stores</h3>
 
         <ul>
-            {searchResults.map((item, i) => (
+            {searchResults.length  &&  searchResults.map((item, i) => (
                 <li key={item.storeID}>{item.storeName}
                     <input onChange={() => handleCheck(item.storeID)} name={'checkbox'} type={'checkbox'}
                            checked={item.isChecked}/>
@@ -206,19 +206,17 @@ const ShopGames = () => {
         {searchGames && searchGames.length && dealsGames.length
 
             ? dealsGames.map((item, i) => (
-                <div key={i}>
+                <div key={item.dealID}>
                     <a href={`https://www.metacritic.com${item.metacriticLink}`} target="_blank">
                         Title:{item.title}
                         {item.metacriticScore}
                         Rating:{item.dealRating}
                          Date: {new Date(item.releaseDate).toLocaleDateString()}
-                        <img src={item.thumb}  alt={item.title} width={100} height={100}/>
+                        <img src={item.thumb}  alt={item.title} width={150} height={100}/>
                     </a>
                 </div>))
 
             : <p>Empty game list</p>
-
-
         }
 
     </div>
